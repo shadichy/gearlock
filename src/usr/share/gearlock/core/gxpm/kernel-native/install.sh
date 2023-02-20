cdbd7605a34e2b6b215e862a825a80a420b207f6
shadichy <hotrobb360web@gmail.com>
2023-02-20 09:28:58 +0700

    new method to install kernel

--- src/usr/share/gearlock/core/gxpm/kernel-native/install.sh
+++ src/usr/share/gearlock/core/gxpm/kernel-native/install.sh
@@ -3,7 +3,6 @@
 ## For proper developer documentation, visit https://supreme-gamers.com/gearlock
 # Check `!zygote.sh` to configure your package functions or gearlock can also guide you during the build process.
 
-
 #####--- Import Functions ---#####
 get_base_dir # Returns execution directory path in $BD variable
 # check_compat 6.8.8 # Returns yes in $COMPAT variable if the user is running at least 6.8 GearLock
@@ -19,7 +18,6 @@ get_base_dir # Returns execution directory path in $BD variable
 # ## Load native scripts
 # # rsync "$CORE/gxpm/kernel-native/uninstall.sh" "$CORE/gxpm/kernel-native/install.sh" "$BD" && exec "$BD/install.sh"
 
-
 ###
 ### Start of un-native installer script.
 ###
@@ -30,27 +28,26 @@ FIRMDIR_OLD="$FIRMDIR.old"
 FIRMDIR_UPDATE="$FIRMDIR.update"
 DALVIKDIR="/data/dalvik-cache"
 PKG_KERNEL_IMAGE="$BD/kernel"
+[ -e "$PKG_KERNEL_IMAGE" ] || PKG_KERNEL_IMAGE="$BD"/system/lib/module/*/kernel
+[ -e "$PKG_KERNEL_IMAGE" ] || PKG_KERNEL_IMAGE="$BD"/system/lib/module/*/vmlinu[zx]
 EFFECTIVE_FIRMDIR_PLACEHOLDER="effective-kernel"
 GBSCRIPT[1]="$GBDIR/init/UpdateKernelFirmware"
 GBSCRIPT[2]="$GBDIR/init/ClearDalvikForKernelUpdate"
 
 # Define functions
-handleError ()
-{
-    
+handleError() {
+
     if [ $? != 0 ]; then
         # Revert back any incomplete changes
         test ! -e "$FIRMDIR" && test -e "$FIRMDIR_OLD" && mv "$FIRMDIR_OLD" "$FIRMDIR"
         geco "\n[!!!] Error: $1" && exit "${2:-101}"
     fi
-    
+
 }
 
-make_gbscript_UpdateFirmware ()
-{
-    
-cat << EOF > "${GBSCRIPT[1]}"
+make_gbscript_UpdateFirmware() {
 
+    cat <<EOF >"${GBSCRIPT[1]}"
 ## Kernel firmware updater gearboot script for live system installation
 #######################################################################
 
@@ -70,13 +67,10 @@ fi
 rm "\$0" # Remove GBSCRIPT
 
 EOF
-    
 }
 
-make_gbscript_clearDalvik ()
-{
-    
-cat << EOF > "${GBSCRIPT[2]}"
+make_gbscript_clearDalvik() {
+    cat <<EOF >"${GBSCRIPT[2]}"
 
 ## Dalvik cache cleaning gearboot script for live system installation
 ######################################################################
@@ -85,26 +79,33 @@ test -d "$DALVIKDIR" && geco "--+ Clearing dalvik-cache, it may take a bit long 
 rm "\$0"
 
 EOF
-    
 }
 
-doJob ()
-{
+clearDalvik() {
+    # Clear dalvik-cache
+    if test "$TERMINAL_EMULATOR" == "no" && test -d "$DALVIKDIR"; then
+        geco "\n+ Clearing dalvik-cache, it may take a bit long on your next boot" && rm -rf "$DALVIKDIR"/*
+    elif test "$TERMINAL_EMULATOR" == "yes" && test -d "$DALVIKDIR"; then
+        make_gbscript_clearDalvik
+    fi
+}
+
+doJob() {
     # Merge files
-    gclone "$BD/system/" "$SYSTEM_DIR"; handleError "Failed to place files"
-    
-	# Install new kernel image
-	kdir=$(ls "$BD"/system/lib/modules/*)
-	kflavor=${kdir##*-}
-    nout rsync "$PKG_KERNEL_IMAGE" "/boot/vmlinuz-$kflavor"; handleError "Failed to update kernel image"
-	mkinitfs "${kdir##*/}"
-	grub-mkconfig -o /boot/grub/grub.cfg
-    
+    gclone "$BD/system/" "$SYSTEM_DIR"
+    handleError "Failed to place files"
+
+    # Install new kernel image
+    kver=$(file -bL "$PKG_KERNEL_IMAGE" | grep -o 'version [^ ]*' | cut -d ' ' -f 2)
+    nout rsync "$PKG_KERNEL_IMAGE" "/boot/vmlinuz-$kver"
+    handleError "Failed to update kernel image"
+    mkinitfs "$kver"
+    update-grub
+
     # Cleanup package firmware before uninstallation script generation (GEN_UNINS)
     test -d "$BD$FIRMDIR" && rm -r "$BD$FIRMDIR"
-    
-}
 
+}
 
 # Warning info for installation from GUI to avoid system crash
 test "$BOOTCOMP" == "yes" && geco "[!!!] You seem to be installing from a live system, best practice is to install from RECOVERY-MODE.\n"
@@ -113,51 +114,53 @@ test "$BOOTCOMP" == "yes" && geco "[!!!] You seem to be installing from a live s
 ! touch -c "$SYSTEM_DIR/lib" >/dev/null 2>&1 && geco "[!!!] $SYSTEM_DIR is not writable, did you ${PINK}SuperCharge${RC} it yet ?" && exit 101
 
 # Main Loop
-if [ -d "$BD$FIRMDIR" ]; then
-    geco "This kernel package also provides additional firmware."
-    while true
-    do
-        read -rn1 -p "$(geco "Do you want to replace system firmware with the ${BLUE}firmware${RC} provided by this kernel package? [${GREEN}Y${RC}/n]") " i
-        case $i in
-            
-            [Yy] ) geco "\n\n+ Placing the kernel module and firmware files into your system"
-                
-                if [ "$TERMINAL_EMULATOR" == "yes" ]; then
-                    make_gbscript_UpdateFirmware; mv "${BD}${FIRMDIR}" "${BD}${FIRMDIR_UPDATE}"; handleError "Failed to rename package firmware to firmware.update"
-                    echo "${NAME}_${VERSION}" > "${BD}${FIRMDIR_UPDATE}/${EFFECTIVE_FIRMDIR_PLACEHOLDER}"; doJob; break
-                else
-                    if [ -e "$FIRMDIR_OLD" ]; then
-                        nout rm -r "$FIRMDIR_OLD"; handleError "Failed to cleanup firmware.old"
-                    fi
-                    if [ -e "$FIRMDIR" ]; then
-                        mv "$FIRMDIR" "$FIRMDIR_OLD"; handleError "Failed to backup old firmware"
-                    fi
-                    doJob; echo "${NAME}_${VERSION}" > "${FIRMDIR}/${EFFECTIVE_FIRMDIR_PLACEHOLDER}"; break
-                fi
-                
-            ;;
-            
-            [Nn] ) geco "\n\n+ Placing the kernel module files into your system"
-                
-                rm -f "${GBSCRIPT[1]}" "${GBSCRIPT[2]}"
-                rm -rf "${BD}${FIRMDIR}"; handleError "Failed to cleanup package firmware"; doJob; break
-                
-            ;;
-            
-            *) geco "\n- Enter either ${GREEN}Y${RC}es or no"
-                
-            ;;
-            
-        esac
-    done
-else
+if [ ! -d "$BD$FIRMDIR" ]; then
     rm -f "${GBSCRIPT[1]}" "${GBSCRIPT[2]}"
-    geco "\n+ Placing the kernel module files into your system" && doJob
+    geco "\n+ Placing the kernel module files into your system"
+    doJob
+    clearDalvik
+    exit 0
 fi
 
-# Clear dalvik-cache
-if test "$TERMINAL_EMULATOR" == "no" && test -d "$DALVIKDIR"; then
-    geco "\n+ Clearing dalvik-cache, it may take a bit long on your next boot" && rm -rf "$DALVIKDIR"/*
-    elif test "$TERMINAL_EMULATOR" == "yes" && test -d "$DALVIKDIR"; then
-    make_gbscript_clearDalvik
-fi
+geco "This kernel package also provides additional firmware."
+while true; do
+    read -rn1 -p "$(geco "Do you want to replace system firmware with the ${BLUE}firmware${RC} provided by this kernel package? [${GREEN}Y${RC}/n]") " i
+    case $i in
+    [Yy])
+        "$BD"/install_firmware.sh
+        geco "\n\n+ Placing the kernel module and firmware files into your system"
+
+        if [ "$TERMINAL_EMULATOR" == "yes" ]; then
+            make_gbscript_UpdateFirmware
+            mv "${BD}${FIRMDIR}" "${BD}${FIRMDIR_UPDATE}"
+            handleError "Failed to rename package firmware to firmware.update"
+            echo "${NAME}_${VERSION}" >"${BD}${FIRMDIR_UPDATE}/${EFFECTIVE_FIRMDIR_PLACEHOLDER}"
+            doJob
+            break
+        else
+            if [ -e "$FIRMDIR_OLD" ]; then
+                nout rm -r "$FIRMDIR_OLD"
+                handleError "Failed to cleanup firmware.old"
+            fi
+            if [ -e "$FIRMDIR" ]; then
+                mv "$FIRMDIR" "$FIRMDIR_OLD"
+                handleError "Failed to backup old firmware"
+            fi
+            doJob
+            echo "${NAME}_${VERSION}" >"${FIRMDIR}/${EFFECTIVE_FIRMDIR_PLACEHOLDER}"
+            break
+        fi
+        ;;
+    [Nn])
+        geco "\n\n+ Placing the kernel module files into your system"
+
+        rm -f "${GBSCRIPT[1]}" "${GBSCRIPT[2]}"
+        rm -rf "${BD}${FIRMDIR}"
+        handleError "Failed to cleanup package firmware"
+        doJob
+        break
+        ;;
+    *) geco "\n- Enter either ${GREEN}Y${RC}es or no" ;;
+    esac
+done
+clearDalvik
--- src/usr/share/gearlock/core/gxpm/kernel-native/uninstall.sh
+++ src/usr/share/gearlock/core/gxpm/kernel-native/uninstall.sh
@@ -18,66 +18,71 @@ DALVIKDIR="/data/dalvik-cache"
 EFFECTIVE_FIRMDIR_PLACEHOLDER="$FIRMDIR/effective-kernel"
 GBSCRIPT[1]="$GBDIR/init/UpdateKernelFirmware"
 GBSCRIPT[2]="$GBDIR/init/ClearDalvikForKernelUpdate"
+PKG_KERNEL_IMAGE=$(grep -E "(kernel|vmlinu[zx])$" "/var/lib/gearlock/pkgs/$NAME/files")
 
 # Define functions
-handleError ()
-{
-    if [ $? != 0 ]; then
-        geco "\n[!!!] Error: $1" && return "${2:-101}"
-    fi
-}
+handleError() { [ $? != 0 ] && geco "\n[!!!] Error: $1" && return "${2:-101}"; }
 
 # Deny uninstallation from GUI to avoid system crash
 if [ "$TERMINAL_EMULATOR" == "yes" ]; then
     geco "\n+ You can not uninstall kernel from Android GUI, it will crash your system."
     geco "+ It is not recommended that you uninstall from a live system, best practice is to uninstall from RECOVERY-MODE."
     geco "+ You can still run GearLock in ${PURPLE}TTY${RC} and uninstall from there but it's not recommended.\n"
-    while true
-    do
+    while true; do
         read -rn1 -p "$(geco "Do you want to switch to ${BGREEN}TTY${RC} and uninstall from there ? [${GREEN}Y${RC}/n]") " i
         case $i in
-            
-            [Yy] ) geco "\n\n+ Switching to TTY GearLock GXPM ..." && sleep 2
+
+        [Yy])
+            geco "\n\n+ Switching to TTY GearLock GXPM ..." && sleep 2
             sudo openvt -s gxpm -u "$UNINS_SCRIPT"
-			return 101 ;;
-            
-            [Nn] ) geco "\n\n+ Okay, uninstallation process will exit"
-            return 101 ;;
-            
-            *) geco "\n- Enter either ${GREEN}Y${RC}es or no" ;;
-            
+            return 101
+            ;;
+
+        [Nn])
+            geco "\n\n+ Okay, uninstallation process will exit"
+            return 101
+            ;;
+
+        *) geco "\n- Enter either ${GREEN}Y${RC}es or no" ;;
+
         esac
     done
 fi
 
 # Check if the user is running uninstall right after installation without reboot (For GUI installation)
-if test -e "$FIRMDIR_UPDATE"; then rm -r "$FIRMDIR_UPDATE"; handleError "Failed to cleanup $(basename "$FIRMDIR_UPDATE")"; fi
+if test -e "$FIRMDIR_UPDATE"; then
+    rm -r "$FIRMDIR_UPDATE"
+    handleError "Failed to cleanup $(basename "$FIRMDIR_UPDATE")"
+fi
 rm -f "${GBSCRIPT[1]}" "${GBSCRIPT[2]}"
 
 # Remove kernel
-kdir=$(ls "$BD"/system/lib/modules/*)
-kflavor=${kdir##*-}
-rm -rf "/boot/vmlinuz-$kflavor" "/boot/initramfs-$kflavor.img"
-grub-mkconfig -o /boot/grub/grub.cfg
+kver=$(file -bL "$PKG_KERNEL_IMAGE" | grep -o 'version [^ ]*' | cut -d ' ' -f 2)
+rm -rf "/boot/vmlinuz-$kver" "/boot/initramfs-$kver.img"
+update-grub
 
 # Restore stock modules/firmware dir
-if [ -d "$FIRMDIR_OLD" ] && [ "$(cat "$EFFECTIVE_FIRMDIR_PLACEHOLDER")"  == "${NAME}_${VERSION}" ]; then
+if [ -d "$FIRMDIR_OLD" ] && [ "$(cat "$EFFECTIVE_FIRMDIR_PLACEHOLDER")" == "${NAME}_${VERSION}" ]; then
     while true; do
         read -rn1 -p "$(geco "Do you want to restore previous firmware version? [${GREEN}Y${RC}/n]") " i
         case $i in
-            [Yy] ) geco "\n+ Restoring stock $(basename "$FIRMDIR") ..."
-                nout rm -r "$FIRMDIR" && mv "$FIRMDIR_OLD" "$FIRMDIR"; handleError "Failed to restore stock $(basename "$FIRMDIR")"
-				break
+        [Yy])
+            geco "\n+ Restoring stock $(basename "$FIRMDIR") ..."
+            nout rm -r "$FIRMDIR" && mv "$FIRMDIR_OLD" "$FIRMDIR"
+            handleError "Failed to restore stock $(basename "$FIRMDIR")"
+            break
             ;;
-            
-            [Nn] ) geco "\n+ Current firmware is kept"
-                break
+
+        [Nn])
+            geco "\n+ Current firmware is kept"
+            break
             ;;
-            
-            *) geco "\n- Enter either ${GREEN}Y${RC}es or no"
-                
+
+        *)
+            geco "\n- Enter either ${GREEN}Y${RC}es or no"
+
             ;;
-            
+
         esac
     done
 fi
